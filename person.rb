2c25f6e002e128fd5dcda5c68ab1d4b67bbfc8b0
Isabellanamutenda <namtendaisabella.com>
2021-12-02 14:56:23 +0300

    fix linters errors

--- person.rb
+++ person.rb
@@ -1,25 +1,26 @@
-require_relative './corrector'
+require './corrector'
 
 class Person
   attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
+    @corrector = Corrector.new()
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new()
-  end
-
-
-  def of_age?
-    @age >= 18
   end
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
+  
+  def of_age?
+    @age >= 18
+  end
+
+  
 
   private :of_age?
 
