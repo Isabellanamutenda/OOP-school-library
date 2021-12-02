e02db482cc9e429f6a6e093039868917f7f66c65
Isabellanamutenda <namtendaisabella.com>
2021-12-02 14:51:06 +0300

    fix required changes

--- person.rb
+++ person.rb
@@ -1,7 +1,7 @@
 require_relative './corrector'
 
 class Person
-  attr_accessor :name, :age, :rentals
+  attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unknown', parent_permission: true)
@@ -9,20 +9,16 @@ class Person
     @name = name
     @age = age
     @parent_permission = parent_permission
-    @corrector = Corrector.new
-    @rentals = []
+    @corrector = Corrector.new()
   end
 
-  def add_rental(_rental)
-    Rental.new(date, self, book)
-  end
 
   def of_age?
     @age >= 18
   end
 
   def validate_name
-    @name = @corrector.correct_name
+    @name = @corrector.correct_name(@name)
   end
 
   private :of_age?
@@ -31,6 +27,3 @@ class Person
     of_age? || parent_permission == true
   end
 end
-
-me = Person.new(14, 'donard')
-p me.validate_name
