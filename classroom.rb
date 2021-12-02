4e9d653700c6bf309225d50859b20fe593ddbecd
Isabellanamutenda <namtendaisabella.com>
2021-12-02 22:50:56 +0300

    Created a class Classroom

---
+++ classroom.rb
@@ -0,0 +1,14 @@
+class Classroom
+    attr_accessor :label
+    attr_reader :students
+  
+    def initialize(label)
+      @label = label
+      @student = []
+    end
+  
+    def add_student(student)
+      @students.push(student)
+    end
+  end
+  
