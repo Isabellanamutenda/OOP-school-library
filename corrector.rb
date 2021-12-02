7613499ac2eb897768fa3070c3c5d91cae4c2bb4
Isabellanamutenda <namtendaisabella.com>
2021-12-02 16:23:48 +0300

    fix linters

--- corrector.rb
+++ corrector.rb
@@ -1,6 +1,7 @@
 class Corrector
   def correct_name(name)
-    name.capitalize!
-    name[0, 10]
+    name = name[0, 10] if name.length > 10
+    name = name.capitalize
+  
   end
 end
--- person.rb
+++ person.rb
@@ -1,28 +1,32 @@
-require './corrector'
+require_relative './corrector'
 
 class Person
-  attr_reader :id
   attr_accessor :name, :age
+  attr_reader :id
 
-  def initialize(age, name = 'Unknown', parent_permission = true)
-    @corrector = Corrector.new()
+  def initialize(age, name = 'Unknown', parent_permission: true)
     @id = Random.rand(1..1000)
     @name = name
     @age = age
     @parent_permission = parent_permission
+    @corrector = Corrector.new()
+  end
+
+
+  def of_age?
+    @age >= 18
   end
 
   def validate_name
     @name = @corrector.correct_name(@name)
   end
 
+  private :of_age?
+
   def can_use_services?
-    is_of_age? || @parent_permission
-  end
-
-  private
-
-  def is_of_age?
-    @age >= 18
+    of_age? || parent_permission == true
   end
 end
+
+me = Person.new(14, 'isabella')
+p me.validate_name
