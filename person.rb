9663f1e2680e8d64aaf0b85ac87b8f16a1721684
Isabellanamutenda <namtendaisabella.com>
2021-12-02 11:26:57 +0300

    Add class person

---
+++ person.rb
@@ -0,0 +1,36 @@
+require_relative './corrector'
+
+class Person
+  attr_accessor :name, :age, :rentals
+  attr_reader :id
+
+  def initialize(age, name = 'Unknown', parent_permission: true)
+    @id = Random.rand(1..1000)
+    @name = name
+    @age = age
+    @parent_permission = parent_permission
+    @corrector = Corrector.new
+    @rentals = []
+  end
+
+  def add_rental(_rental)
+    Rental.new(date, self, book)
+  end
+
+  def of_age?
+    @age >= 18
+  end
+
+  def validate_name
+    @name = @corrector.correct_name
+  end
+
+  private :of_age?
+
+  def can_use_services?
+    of_age? || parent_permission == true
+  end
+end
+
+me = Person.new(14, 'donard')
+p me.validate_name
