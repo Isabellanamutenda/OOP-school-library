ec0f631492d81c33b0e0784d73fdf6d440c9345a
Isabellanamutenda <namtendaisabella.com>
2021-12-08 17:47:46 +0300

    Update main.rb file

--- student.rb
+++ student.rb
@@ -1,17 +1,13 @@
-require './person'
+# rubocop: disable Style/OptionalBooleanParameter
+require_relative './person'
+
 class Student < Person
-  attr_reader :classroom
-
-  def classroom=(studentclass)
-    @classroom = studentclass
-    studentclass.students.push(self) unless studentclass.students.include?(self)
-  end
-
-  def initialize(age, name = 'Unknown', parent_permission: true)
-    super(age, name, parent_permission: parent_permission)
+  def initialize(age, name = 'Unknown', parent_permission = true)
+    super(age, name, parent_permission)
   end
 
   def play_hooky
     "¯\(ツ)/¯"
   end
 end
+# rubocop: enable Style/OptionalBooleanParameter
